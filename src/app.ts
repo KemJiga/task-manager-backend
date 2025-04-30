93227a67c08fbc9a071e11d40505f480fd6365bc
Kemjiga <kemerjg@gmail.com>
2025-04-30 17:38:19 -0500

    #2 - add morgan dev dependecie and config prettier

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 // Test route
 app.get('/', (req: Request, res: Response) => {
